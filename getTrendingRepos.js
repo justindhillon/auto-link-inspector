1343939e08a94aad891ad1ef910d6610f81a4851
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-02-01 19:41:15 -0800

    add main.py

--- getTrendingRepos.js
+++
@@ -1,7 +0,0 @@
-import cheerio from 'cheerio';
-import fetch from 'node-fetch';
-
-const data = await fetch('https://github.com/trending');
-const $ = cheerio.load(await data.text());
-
-console.log($('a').find('a'));
---
+++ main.py
@@ -0,0 +1,42 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+def load_from_json(filename):
+    try:
+        with open(filename, 'r') as json_file:
+            return json.load(json_file)
+    except FileNotFoundError:
+        return []
+
+def remove_existing_strings(new_array, existing_array):
+    return [item for item in new_array if item not in existing_array]
+
+URL = "https://github.com/trending"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+articles = soup.find_all("article")
+
+new_array = []
+
+for article in articles:
+    articl = article.find_all("h2")
+    for artic in articl:
+        arti = artic.find_all("a")
+        for art in arti:
+            link = art["href"]
+            new_array.append("https://github.com" + link)
+
+json_filename = "data.json"
+
+existing_array = load_from_json(json_filename)
+filtered_array = remove_existing_strings(new_array, existing_array)
+
+save_to_json(existing_array + filtered_array, json_filename)
+
+print(filtered_array)
